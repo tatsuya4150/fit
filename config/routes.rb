3bbb80d14f803dd925fae600c329be1bead84346
TATSUYA <tatsuya415077@gmail.com>
2017-03-27 22:06:37 +0900

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :tests
   devise_for :users
+  root 'tests#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
