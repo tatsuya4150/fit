a40ff6f206df0fc1c3dfb5712e3555dc10603bb2
TATSUYA <tatsuya415077@gmail.com>
2017-03-27 22:58:35 +0900

    tests for Facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :tests
-  devise_for :users
-  root 'tests#index'
+  root 'tests#inde'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
